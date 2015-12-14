b5a5b0b556d064f7c4f0b621dace0820ef7e485d
Saroar <saroar9@gmail.com>
2015-12-14 13:44:07 +0300

    rails app rename

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_consultant_session'
+Rails.application.config.session_store :cookie_store, key: '_rorqual_consultant_session'
